c49378479a46d1afbf3abd86a7402430d2dd2fb8
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-07-21 22:57:55 +0200

    channel stub

--- src/Goroutines.hs
+++ src/Goroutines.hs
@@ -1,16 +1,38 @@
 module Goroutines where
 
-data Goroutine a = Goroutine a -- to be expanded
+-- this will probably need more type parameters for the current internal state? How else can it hold handles to Channels of different types?
+data Goroutine a = Goroutine a
 
 instance Functor Goroutine where
-	fmap f (Goroutine x) = Goroutine $ f x
+  fmap f (Goroutine x) = Goroutine $ f x
 
 instance Applicative Goroutine where
-	pure = Goroutine
-	(Goroutine f) <*> (Goroutine x) = Goroutine $ f x
+  pure = Goroutine
+  (Goroutine f) <*> (Goroutine x) = Goroutine $ f x
 
 instance Monad Goroutine where
-	(Goroutine x) >>= f = f x
+  (Goroutine x) >>= f = f x
 
 runGoroutine :: Goroutine a -> a
 runGoroutine (Goroutine x) = x
+
+go :: Goroutine a -> Goroutine ()
+go (Goroutine x) = Goroutine ()
+
+data ReadChannel a = ReadChannel
+data WriteChannel a = WriteChannel
+
+mkChannel :: Int -> Goroutine (ReadChannel a, WriteChannel a)
+mkChannel capacity = return (ReadChannel, WriteChannel)
+
+readC :: ReadChannel a -> Goroutine (Maybe a)
+readC c = return Nothing
+
+writeC :: WriteChannel a -> a -> Goroutine Bool
+writeC c a = return False
+
+-- returns whether the close was successful, i.e. the Channel was not yet closed
+close :: WriteChannel a -> Goroutine Bool
+close c = return True
+
+-- do we need a close for ReadChannel?
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,23 @@
 module Main where
 
+import Data.Maybe (isJust, fromJust)
 import Goroutines
+
+takeWhileJustM :: Monad m => (a -> Maybe b) -> m a -> m [b]
+takeWhileJustM unpack action = fmap reverse $ takeWhileJustM' unpack action []
+  where
+    takeWhileJustM' unpack action xs = do
+      y <- action
+      case unpack y of
+        (Just x) -> takeWhileJustM' unpack action (x:xs)
+        Nothing -> return xs
 
 main :: IO ()
 main = do
-  putStrLn $ runGoroutine $ do
-    return "hello world"
+  putStrLn $ unlines $ runGoroutine $ do
+    (r, w) <- mkChannel 0
+    go $ do
+      writeC w "hello"
+      writeC w "world"
+      close w
+    takeWhileJustM id (readC r)
